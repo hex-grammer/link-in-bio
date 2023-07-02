40662784aba54cb8a6d43b0379eb4b594720c9b7
Rizal Iswandy <=>
2023-07-02 17:49:19 +0800

    profile set-up

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,9 +1,5 @@
 import LynkLogo from "../../public/lynk.svg";
 
 export default function Credits() {
-  return (
-    <div className={"credits"}>
-      <LynkLogo />
-    </div>
-  );
+  return <div className={"credits"}>{/* <LynkLogo /> */}</div>;
 }
